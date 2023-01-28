bff85c92610cd33703bd133a236c0537bf1a7a8c
carodaems <r0889345@student.thomasmore.be>
2023-01-28 22:23:05 +0100

    removed "/"

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ origins = [
     "https://gregarious-buttercream-79d640",
     "https://63d57fc4af3368398f3afe7e--gregarious-buttercream-79d640",
     "https://63d57fc4af3368398f3afe7e--gregarious-buttercream-79d640.netlify.app",
-    "https://quiz-app-interface.netlify.app/",
+    "https://quiz-app-interface.netlify.app",
     "https://quiz-app-interface"
 ]
 
