8206178cf6b1c86360aae08143dffdda3ae4c0d7
carodaems <r0889345@student.thomasmore.be>
2023-01-28 15:02:44 +0100

    bug fixes

--- app/main.py
+++ app/main.py
@@ -55,47 +55,9 @@ def get_db():
         db.close()
 
 
-@app.get("/quiz_rounds")
-def read_quiz_rounds(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    quiz_rounds = db.query(models.QuizRound).offset(skip).limit(limit).all()
-    return quiz_rounds
+# Endpoints
 
-
-@app.post("/quiz_rounds")
-def create_quiz_round(round: schemas.QuizRoundCreate, db: Session = Depends(get_db)):
-    db_round = models.QuizRound(name=round.name, key=round.key)
-    round_check = db.query(models.QuizRound).filter(
-        models.QuizRound.name == round.name).first()
-    if round_check is None:
-        db.add(db_round)
-        db.flush()
-        db.commit()
-        db.refresh(db_round)
-    elif db_round.name == round_check.name:
-        raise HTTPException(
-            status_code=400, detail="This round already exists")
-    return db_round
-
-
-@app.delete("quiz_rounds/{round_id}")
-def delete_quiz_round(round_id: int, db: Session = Depends(get_db)):
-    db_round = db.query(models.QuizRound.filter(
-        models.QuizRound.id == round_id)).first()
-    if db_round is None:
-        raise HTTPException(status_code=404, detail="Quiz round not found")
-    db.delete(db_round)
-    db.commit()
-    return {"message": "Round deleted"}
-
-
-@app.get("/quiz_rounds/{round_id}")
-def read_quiz_round(round_id: int, db: Session = Depends(get_db)):
-    db_round = db.query(models.QuizRound).filter(
-        models.QuizRound.id == round_id).first()
-    if db_round is None:
-        raise HTTPException(status_code=404, detail="Quiz round not found")
-    return db_round
-
+# PUT
 
 @app.put("/quiz_rounds/{round_id}")
 def update_quiz_round(round_id: int, round: schemas.QuizRoundUpdate, db: Session = Depends(get_db)):
@@ -111,6 +73,151 @@ def update_quiz_round(round_id: int, round: schemas.QuizRoundUpdate, db: Session
     return db_round
 
 
+@app.put("/quiz_rounds/questions/check")
+def update_response(guess: schemas.QuestionCompare, db: Session = Depends(get_db)):
+    question = db.query(models.Question).filter(
+        models.Question.question_number == current_round["question"], models.Question.round_id == current_round["round"]).first()
+
+    team = db.query(models.Team).filter(
+        models.Team.id == guess.team_id).first()
+    if team is None:
+        raise HTTPException(status_code=400, detail="Team not found")
+
+    if question.correct_answer == guess.guess:
+        is_correct = True
+    else:
+        is_correct = False
+
+    db_number = db.query(models.Responses).filter(
+        models.Responses.team_id == guess.team_id, models.Responses.question_number == question.question_number, models.Responses.round_id == question.round_id
+    ).first()
+
+    if db_number is None:
+        db_number = models.Responses(
+            reponse=guess.guess, team_id=guess.team_id, correct=is_correct, round_id=question.round_id, question_number=question.question_number)
+    else:
+        db_number.reponse = guess.guess
+        db_number.team_id = guess.team_id
+        db_number.correct = is_correct
+        db_number.round_id = question.round_id
+        db_number.question_number = question.question_number
+        db_number.id = db_number.id
+
+    db.add(db_number)
+    db.flush()
+    db.commit()
+    db.refresh(db_number)
+    return db_number
+
+
+@app.put("/teams/")
+def create_team(team: schemas.TeamCreate, db: Session = Depends(get_db)):
+    name = "Team " + str(team.number)
+
+    db_team = db.query(models.Team).filter(
+        models.Team.id == team.number, models.Team.team_name == name).first()
+    if db_team is None:
+        db_team = models.Team(team_name=name, id=team.number)
+    else:
+        db_team.id = team.number
+        db_team.team_name = name
+
+    db.add(db_team)
+    db.flush()
+    db.commit()
+    db.refresh(db_team)
+    return db_team
+
+
+@app.put("/scores")
+def update_scores(db: Session = Depends(get_db)):
+    teams = db.query(models.Team).all()
+    round_id = current_round["round"]
+    for team in teams:
+        for scores in team.scores:
+            score = scores.score
+            for response in team.responses:
+                if response.correct and response.round_id == round_id:
+                    score += 1
+            team.scores.score = score
+            db.add(scores)
+            db.flush()
+            db.commit()
+            db.refresh(scores)
+    current_round["round"] += 1
+    return {"message": "updated"}
+
+
+@app.put("/scores/{team_id}/increment")
+def increment_score(team_id: int, db: Session = Depends(get_db)):
+    team = db.query(models.Scores).filter(
+        models.Scores.team_id == team_id).first()
+    if not team:
+        raise HTTPException(status_code=404, detail="Team not found")
+    team.score += 1
+    db.commit()
+    db.refresh(team)
+    return {"detail": "Score incremented for team"}
+
+
+@app.put("/scores/{team_id}/decrement")
+def decrement_score(team_id: int, db: Session = Depends(get_db)):
+    team = db.query(models.Scores).filter(
+        models.Scores.team_id == team_id).first()
+    if not team:
+        raise HTTPException(status_code=404, detail="Team not found")
+    team.score -= 1
+    db.commit()
+    db.refresh(team)
+    return {"detail": "Score decremented for team"}
+
+
+@app.put("/quiz_rounds/{round_id}/answers")
+def update_questions(round_id: int, questions: List[schemas.QuestionUpdates], db: Session = Depends(get_db)):
+    db_questions = db.query(models.Question).filter(
+        models.Question.round_id == round_id).all()
+    if not db_questions:
+        raise HTTPException(
+            status_code=404, detail="Round not found or no question for this round")
+    for db_question in db_questions:
+        for question in questions:
+            if db_question.question_number == question.question_number:
+                db_question.correct_answer = question.answer
+                db.add(db_question)
+                db.commit()
+                db.refresh(db_question)
+    return {"detail": "questions updated"}
+
+
+@app.put("/quiz_rounds/{round_id}/questions/{question_id}")
+def update_question(round_id: int, question_id: int, question: schemas.QuestionUpdate, db: Session = Depends(get_db)):
+    db_question = db.query(models.Question).filter(
+        models.Question.id == question_id, models.Question.round_id == round_id).first()
+    if db_question is None:
+        raise HTTPException(status_code=404, detail="Question not found")
+    db_question.correct_answer = question.correct_answer
+    db.add(db_question)
+    db.commit()
+    db.refresh(db_question)
+    return db_question
+
+
+# GET
+@app.get("/quiz_rounds")
+def read_quiz_rounds(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    quiz_rounds = db.query(models.QuizRound).offset(skip).limit(limit).all()
+    return quiz_rounds
+
+
+@app.get("/quiz_rounds/{round_id}")
+def read_quiz_round(round_id: int, db: Session = Depends(get_db)):
+    db_round = db.query(models.QuizRound).filter(
+        models.QuizRound.id == round_id).first()
+    if db_round is None:
+        raise HTTPException(status_code=404, detail="Quiz round not found")
+    return db_round
+
+
 @app.get("/quiz_rounds/{round_id}/questions")
 def read_questions(round_id: int, db: Session = Depends(get_db)):
     questions = db.query(models.Question).filter(
@@ -118,6 +225,97 @@ def read_questions(round_id: int, db: Session = Depends(get_db)):
     if not questions:
         raise HTTPException(status_code=404, detail="Questions not found")
     return questions
+
+
+@app.get("/quiz_rounds/{round_id}/questions/{question_id}")
+def read_question(round_id: int, question_id: int, db: Session = Depends(get_db)):
+    question = db.query(models.Question).filter(
+        models.Question.id == question_id, models.Question.round_id == round_id).first()
+    if question is None:
+        raise HTTPException(status_code=404, detail="Question not found")
+    return question
+
+
+@app.get("/quiz_rounds/questions/check/{team_id}")
+def check_responses(team_id: int, db: Session = Depends(get_db)):
+    responses = db.query(models.Responses).filter(
+        models.Responses.team_id == team_id).all()
+    if not responses:
+        raise HTTPException(status_code=404, detail="Responses not found")
+    return responses
+
+
+@app.get("/teams/")
+def get_teams(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    teams = db.query(models.Team).offset(skip).limit(limit).all()
+    return teams
+
+
+@app.get("/scores")
+def get_scores(db: Session = Depends(get_db)):
+    db_scores = db.query(models.Scores).all()
+
+    scores = {}
+    for score in db_scores:
+        scores[score.team.team_name] = score.score
+
+    sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    converted_scores = dict(sorted_scores)
+
+    return converted_scores
+
+
+@app.get("/scores/raw")
+def get_scores_raw(db: Session = Depends(get_db)):
+    scores = db.query(models.Scores).all()
+    return scores
+
+
+@app.get("/getround")
+async def get_round():
+    return current_round
+
+
+# DELETE
+
+@app.delete("quiz_rounds/{round_id}")
+def delete_quiz_round(round_id: int, db: Session = Depends(get_db)):
+    db_round = db.query(models.QuizRound.filter(
+        models.QuizRound.id == round_id)).first()
+    if db_round is None:
+        raise HTTPException(status_code=404, detail="Quiz round not found")
+    db.delete(db_round)
+    db.commit()
+    return {"message": "Round deleted"}
+
+
+@app.delete("/quiz_rounds/{round_id}/questions/{question_id}")
+def delete_question(round_id: int, question_id: int, db: Session = Depends(get_db)):
+    question = db.query(models.Question).filter(
+        models.Question.id == question_id, models.Question.round_id == round_id).first()
+    if question is None:
+        raise HTTPException(status_code=404, detail="Question not found")
+    db.delete(question)
+    db.commit()
+    return {"message": "Question deleted"}
+
+
+# POST
+
+@app.post("/quiz_rounds")
+def create_quiz_round(round: schemas.QuizRoundCreate, db: Session = Depends(get_db)):
+    db_round = models.QuizRound(name=round.name, key=round.key)
+    round_check = db.query(models.QuizRound).filter(
+        models.QuizRound.name == round.name).first()
+    if round_check is None:
+        db.add(db_round)
+        db.flush()
+        db.commit()
+        db.refresh(db_round)
+    elif db_round.name == round_check.name:
+        raise HTTPException(
+            status_code=400, detail="This round already exists")
+    return db_round
 
 
 @app.post("/quiz_rounds/{round_id}/questions")
@@ -164,39 +362,6 @@ async def create_multiple_quiz_answers(round_id: int, answers: List[schemas.Ques
     return {"status": "success"}
 
 
-@app.get("/quiz_rounds/{round_id}/questions/{question_id}")
-def read_question(round_id: int, question_id: int, db: Session = Depends(get_db)):
-    question = db.query(models.Question).filter(
-        models.Question.id == question_id, models.Question.round_id == round_id).first()
-    if question is None:
-        raise HTTPException(status_code=404, detail="Question not found")
-    return question
-
-
-@app.put("/quiz_rounds/{round_id}/questions/{question_id}")
-def update_question(round_id: int, question_id: int, question: schemas.QuestionUpdate, db: Session = Depends(get_db)):
-    db_question = db.query(models.Question).filter(
-        models.Question.id == question_id, models.Question.round_id == round_id).first()
-    if db_question is None:
-        raise HTTPException(status_code=404, detail="Question not found")
-    db_question.correct_answer = question.correct_answer
-    db.add(db_question)
-    db.commit()
-    db.refresh(db_question)
-    return db_question
-
-
-@app.delete("/quiz_rounds/{round_id}/questions/{question_id}")
-def delete_question(round_id: int, question_id: int, db: Session = Depends(get_db)):
-    question = db.query(models.Question).filter(
-        models.Question.id == question_id, models.Question.round_id == round_id).first()
-    if question is None:
-        raise HTTPException(status_code=404, detail="Question not found")
-    db.delete(question)
-    db.commit()
-    return {"message": "Question deleted"}
-
-
 @app.post("/quiz_rounds/questions/check")
 def compare_question(guess: schemas.QuestionCompare, db: Session = Depends(get_db)):
     question = db.query(models.Question).filter(
@@ -214,138 +379,6 @@ def compare_question(guess: schemas.QuestionCompare, db: Session = Depends(get_d
     return db_response
 
 
-@app.put("/quiz_rounds/questions/check")
-def update_response(guess: schemas.QuestionCompare, db: Session = Depends(get_db)):
-    question = db.query(models.Question).filter(
-        models.Question.question_number == current_round["question"], models.Question.round_id == current_round["round"]).first()
-
-    team = db.query(models.Team).filter(
-        models.Team.id == guess.team_id).first()
-    if team is None:
-        raise HTTPException(status_code=400, detail="Team not found")
-
-    if question.correct_answer == guess.guess:
-        is_correct = True
-    else:
-        is_correct = False
-
-    db_number = db.query(models.Responses).filter(
-        models.Responses.team_id == guess.team_id, models.Responses.question_number == question.question_number, models.Responses.round_id == question.round_id
-    ).first()
-
-    if db_number is None:
-        db_number = models.Responses(
-            reponse=guess.guess, team_id=guess.team_id, correct=is_correct, round_id=question.round_id, question_number=question.question_number)
-    else:
-        db_number.reponse = guess.guess
-        db_number.team_id = guess.team_id
-        db_number.correct = is_correct
-        db_number.round_id = question.round_id
-        db_number.question_number = question.question_number
-        db_number.id = db_number.id
-
-    db.add(db_number)
-    db.flush()
-    db.commit()
-    db.refresh(db_number)
-    return db_number
-
-
-@app.get("/quiz_rounds/questions/check/{team_id}")
-def check_responses(team_id: int, db: Session = Depends(get_db)):
-    responses = db.query(models.Responses).filter(
-        models.Responses.team_id == team_id).all()
-    if not responses:
-        raise HTTPException(status_code=404, detail="Responses not found")
-    return responses
-
-
-@app.put("/teams/")
-def create_team(team: schemas.TeamCreate, db: Session = Depends(get_db)):
-    name = "Team " + str(team.number)
-
-    db_team = db.query(models.Team).filter(
-        models.Team.id == team.number, models.Team.team_name == name).first()
-    if db_team is None:
-        db_team = models.Team(team_name=name, id=team.number)
-    else:
-        db_team.id = team.number
-        db_team.team_name = name
-
-    db.add(db_team)
-    db.flush()
-    db.commit()
-    db.refresh(db_team)
-    return db_team
-
-
-@app.get("/teams/")
-def get_teams(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    teams = db.query(models.Team).offset(skip).limit(limit).all()
-    return teams
-
-
-@app.get("/scores")
-def get_scores(db: Session = Depends(get_db)):
-    db_scores = db.query(models.Scores).all()
-
-    scores = {}
-    for score in db_scores:
-        scores[score.team.team_name] = score.score
-
-    sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
-    converted_scores = dict(sorted_scores)
-
-    return converted_scores
-
-
-@app.get("/scores/raw")
-def get_scores_raw(db: Session = Depends(get_db)):
-    scores = db.query(models.Scores).all()
-
-    return scores
-
-
-@app.put("/scores")
-def set_scores(db: Session = Depends(get_db)):
-    teams = db.query(models.Team).all()
-    for team in teams:
-        score = 0
-        for response in team.responses:
-            if response.correct:
-                score += 1
-        scores = models.Scores(team_id=team.id, score=score)
-        db.add(scores)
-        db.flush()
-        db.commit()
-        db.refresh(scores)
-    return {"message": "success"}
-
-
-@app.put("/scores/{team_id}/increment")
-def increment_score(team_id: int, db: Session = Depends(get_db)):
-    team = db.query(models.Scores).filter(
-        models.Scores.team_id == team_id).first()
-    if not team:
-        raise HTTPException(status_code=404, detail="Team not found")
-    team.score += 1
-    db.commit()
-    db.refresh(team)
-    return {"detail": "Score incremented for team"}
-
-
-@app.put("/scores/{team_id}/decrement")
-def decrement_score(team_id: int, db: Session = Depends(get_db)):
-    team = db.query(models.Scores).filter(
-        models.Scores.team_id == team_id).first()
-    if not team:
-        raise HTTPException(status_code=404, detail="Team not found")
-    team.score -= 1
-    db.commit()
-    db.refresh(team)
-    return {"detail": "Score decremented for team"}
-
-
 @app.post("/setround")
 async def set_round():
     current_round["round"] += 1
@@ -356,25 +389,3 @@ async def set_round():
 async def set_question():
     current_round["question"] += 1
     return current_round
-
-
-@app.get("/getround")
-async def get_round():
-    return current_round
-
-
-@app.put("/quiz_rounds/{round_id}/answers")
-def update_questions(round_id: int, questions: List[schemas.QuestionUpdates], db: Session = Depends(get_db)):
-    db_questions = db.query(models.Question).filter(
-        models.Question.round_id == round_id).all()
-    if not db_questions:
-        raise HTTPException(
-            status_code=404, detail="Round not found or no question for this round")
-    for db_question in db_questions:
-        for question in questions:
-            if db_question.question_number == question.question_number:
-                db_question.correct_answer = question.answer
-                db.add(db_question)
-                db.commit()
-                db.refresh(db_question)
-    return {"detail": "questions updated"}
